034997fb5de0d8bfc91e65cdf2a25057df0a192f
Benjamin Vasseur <benjaminvasseur@lviscorp.com>
2024-11-09 19:43:32 +0900

    set keyboard camera control

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const Scene: React.FC = () => {
   const mountRef = useRef<HTMLDivElement | null>(null);
+
+  const initCamPos = { x: 4, y: 2, z: 0 };
 
   useEffect(() => {
     if (!mountRef.current) return;
@@ -22,21 +23,46 @@ const Scene: React.FC = () => {
       0.1,
       1000,
     );
-    camera.position.set(5, 2, 0.5);
+    camera.position.set(initCamPos.x, initCamPos.y, initCamPos.z);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const controls = new OrbitControls(camera, renderer.domElement); // controls will be needed later for interface i guess
+    /**
+     * Camera controls
+     */
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.keyPanSpeed = 25;
+    // Set WASD shortcut
+    controls.keys = {
+      LEFT: "KeyA",
+      UP: "KeyW",
+      RIGHT: "KeyD",
+      BOTTOM: "KeyS",
+    };
+    controls.saveState();
+
+    controls.listenToKeyEvents(window);
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        // Space bar to reset camera position
+        case " ":
+          controls.reset();
+          break;
+        default:
+          break;
+      }
+      controls.update();
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
 
     /**
      * Grid management
      */
-    // const gridHelper = new THREE.GridHelper(10, 10);
-    // scene.add(gridHelper);
-
     const radius = 10;
     const sectors = 16;
     const rings = 8;
@@ -72,9 +98,7 @@ const Scene: React.FC = () => {
      * Animate (rendering)
      */
     const animate = () => {
-      renderer.render(scene, camera);
-      // cube.rotation.x += 0.01;
-      // cube.rotation.y += 0.01;
+      controls.update();
       renderer.render(scene, camera);
     };
     renderer.setAnimationLoop(animate);
